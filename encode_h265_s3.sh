73140faf8d3de6552a38725d6ce875895c184819
Sumin Byeon <suminb@gmail.com>
2022-06-17 09:08:50 +0000

    Encode directly from/to S3

---
+++ encode_h265_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+endpoint="http://hyperion.shortbread.io:8000"
+source=$1
+basename=$(basename "${source%.*}")
+dirname=$(dirname "$source")
+target=${dirname}/${basename}_H265.mp4
+# https://trac.ffmpeg.org/wiki/Encode/H.265
+# NOTE: -tag:v hvc1 makes possible to import the file into Final Cut Pro
+
+if [[ "$source" = *_H265.mp4 || "$source" = *_Original.mp4 ]]; then
+    echo "${source} is already processed. Aborted."
+    exit 1
+fi
+
+
+time ffmpeg -i $(aws --endpoint-url=$endpoint s3 presign "$source") -c:v libx265 -crf 23 -preset fast -c:a aac -b:a 128k -map_metadata 0 -tag:v hvc1 -y -f mp4 -movflags frag_keyframe+empty_moov pipe:1 | aws --endpoint-url=$endpoint s3 cp - $target
