94fd97482819f2e53e3561e92b7e04db99251e12
Sumin Byeon <suminb@gmail.com>
2022-10-05 14:28:56 +0900

    Add double quotes

--- encode_h265_s3.sh
+++ encode_h265_s3.sh
@@ -13,5 +13,5 @@ if [[ "$source" = *_H265.mp4 || "$source" = *_Original.mp4 ]]; then
     exit 1
 fi
 
-ffmpeg -i $(aws --endpoint-url=$endpoint s3 presign "$source") -c:v libx265 -crf 23 -preset fast -c:a aac -b:a 128k -map_metadata 0 -tag:v hvc1 -y -f mp4 -movflags frag_keyframe+empty_moov pipe:1 | aws --endpoint-url=$endpoint s3 cp - $target && \
-    aws --endpoint-url=$endpoint s3 rm $source
+ffmpeg -i $(aws --endpoint-url=$endpoint s3 presign "$source") -c:v libx265 -crf 23 -preset fast -c:a aac -b:a 128k -map_metadata 0 -tag:v hvc1 -y -f mp4 -movflags frag_keyframe+empty_moov pipe:1 | aws --endpoint-url=$endpoint s3 cp - "$target" && \
+    aws --endpoint-url=$endpoint s3 rm "$source"
