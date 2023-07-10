56c9ae779c71de46b623aa421a6481088c84bef1
Sumin Byeon <suminb@gmail.com>
2023-07-10 19:34:40 +0900

    feat: Revise FCP backup script

--- backup_fcp.sh
+++ backup_fcp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-path1="/Volumes/T7/White Rabbit (T7).fcpbundle"
-tar -cvf - "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://backup/white-rabbit-t7/$FILE'
+#path1="/Volumes/T7/White Rabbit (T7).fcpbundle"
+#tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-t7/$FILE'
 
 path2="/Volumes/Backup2/White Rabbit (archived).fcpbundle"
-tar -cvf - "$path2" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://backup/white-rabbit-archived/$FILE'
+tar -cvf - "$path2" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-archived/$FILE'
