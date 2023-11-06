aca71d7a98ba848ea92cf9787f6b0dab434f4245
Sumin Byeon <suminb@gmail.com>
2023-11-07 00:49:52 +0900

    feat: Split non-consecutive clips

--- consolidate.py
+++ consolidate.py
@@ -2,11 +2,11 @@
 rear).
 """
 
-
 from datetime import datetime
 import glob
 import os
 import subprocess
+import sys
 
 from typing import List
 
@@ -18,15 +18,15 @@ def parse_datetime(filename):
     return datetime.strptime(filename[4:-6], "%Y_%m_%d_%H_%M_%S")
 
 
-def consolidate_by_date_and_category(paths: List[str]) -> dict:
+def consolidate_by_date_and_direction(paths: List[str]) -> dict:
     consolidated = {}
     for path in paths:
         filename = os.path.basename(path)
         dt = parse_datetime(filename)
         date_str = dt.strftime("%Y%m%d")
-        category = filename[-len("F.mp4")]
+        direction = filename[-len("F.mp4")]
 
-        key = (date_str, category)
+        key = (date_str, direction)
 
         consolidated.setdefault(key, [])
         consolidated[key].append(path)
@@ -35,6 +35,42 @@ def consolidate_by_date_and_category(paths: List[str]) -> dict:
         consolidated[key] = sorted(consolidated[key])
 
     return consolidated
+
+
+def is_consecutive(prev_min, curr_min):
+    return 0 <= (curr_min - prev_min) <= 2 or prev_min + 1 == curr_min + 60
+
+
+def split_non_consecutive_filenames(consolidated: dict):
+    splitted = {}
+    for key, value in consolidated.items():
+        date, direction = key
+        sequence = 0
+
+        key = (date, direction, sequence)
+        splitted.setdefault(key, [])
+
+        for prev, curr in zip([None] + value[:-1], value):
+            if prev is not None:
+                prev_dt = parse_datetime(os.path.basename(prev))
+                curr_dt = parse_datetime(os.path.basename(curr))
+
+                if not is_consecutive(prev_dt.minute, curr_dt.minute):
+                    sequence += 1
+                    key = (date, direction, sequence)
+                    splitted.setdefault(key, [])
+
+            splitted[key].append(curr)
+
+    return splitted
+
+
+def add_null_sequence_number(consolidated: dict):
+    with_seq = {}
+    for (date, direction), value in consolidated.items():
+        key = (date, direction, 0)
+        with_seq[key] = value
+    return with_seq
 
 
 def generate_concat_file(paths: List[str], concat_file_path: str):
@@ -56,17 +92,31 @@ def generate_concat_file(paths: List[str], concat_file_path: str):
 
 
 if __name__ == "__main__":
-    base_path = "/Volumes/archive/Others/아이나비/Driving"
-    consolidated = consolidate_by_date_and_category(
+    # Rule of thumb: split non-consecutive footages for driving;
+    # no split for parking footages
+    # base_path = "/Volumes/archive/Others/inavi/Driving"
+    base_path = sys.argv[1]
+    consolidated = consolidate_by_date_and_direction(
         glob.glob(os.path.join(base_path, "*.MP4"))
     )
-    for key, value in consolidated.items():
-        date, category = key
+    split_non_consecutive = True
 
-        concat_filename = f"/tmp/concat_{date}_{category}.txt"
+    if split_non_consecutive:
+        consolidated = split_non_consecutive_filenames(consolidated)
+    else:
+        consolidated = add_null_sequence_number(consolidated)
+
+    for key, value in consolidated.items():
+        date, direction, sequence = key
+
+        concat_filename = f"/tmp/concat_{date}_{direction}_{sequence}.txt"
         generate_concat_file(value, concat_filename)
 
-        output_filename = os.path.join(base_path, f"{date}_{category}.mp4")
+        output_filename = os.path.join(base_path, f"{date}_{direction}_{sequence}.mp4")
+        if os.path.exists(output_filename):
+            print(f"{output_filename} already exists. Skipping...")
+            continue
+
         command = (
             f"ffmpeg -f concat -safe -0 -i {concat_filename} -c copy {output_filename}"
         )
