2ee1b5549df29c02339d8be2123d92bd0494527b
Sumin Byeon <suminb@gmail.com>
2024-05-09 18:33:49 +0900

    feat: Revise backup source path

--- backup_fcp.sh
+++ backup_fcp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#path1="/Volumes/T7/White Rabbit (T7).fcpbundle"
-#tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-t7/$FILE'
+path1="/Volumes/T7/White Rabbit 2023.fcpbundle"
+# tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-t7/$FILE'
+tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=https://kr.object.ncloudstorage.com --profile=ncloud s3 cp - s3://archive.shortbread.io/backup/white-rabbit-2023/$FILE'
 
-path2="/Volumes/Backup2/White Rabbit (archived).fcpbundle"
-tar -cvf - "$path2" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-archived/$FILE'
+#path2="/Volumes/Backup2/White Rabbit (archived).fcpbundle"
+#tar -cvf - "$path2" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-archived/$FILE'
