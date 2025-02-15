283c217054c3c124b8179a25bb701a8a0475e6bf
Sumin Byeon <suminb@gmail.com>
2025-02-15 16:48:33 +0900

    feat: Split by year

--- backup_fcp.sh
+++ backup_fcp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-path1="/Volumes/T7/White Rabbit 2023.fcpbundle"
+year=2024
+path1="/Volumes/T7/White Rabbit $year.fcpbundle"
 # tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-t7/$FILE'
-tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=https://kr.object.ncloudstorage.com --profile=ncloud s3 cp - s3://archive.shortbread.io/backup/white-rabbit-2023/$FILE'
+tar -C /Volumes/T7 --zstd -cv "$path1" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=https://kr.object.ncloudstorage.com --profile=ncloud s3 cp - s3://archive.shortbread.io/backup/white-rabbit-$year/$FILE'
 
 #path2="/Volumes/Backup2/White Rabbit (archived).fcpbundle"
 #tar -cvf - "$path2" | gsplit -d -b 4G -a 4 - part_ --filter='aws --endpoint-url=http://hyperion.shortbread.io:8000 s3 cp - s3://archive/white-rabbit-archived/$FILE'
